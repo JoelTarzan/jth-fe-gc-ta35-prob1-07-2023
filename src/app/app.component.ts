a72688b036a9104c2a82a827515e7d4c18799112
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-06 17:12:29 +0200

    Ejercicio acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jth-fe-gc-ta35-prob1-07-2023';
+  
+  nombre: string = '';
+  cif: string = '';
+  direccion: string = '';
+  grupo: number | string | undefined = 'Sin definir';
+
+  clientes: any[] = [];
+
+  // Guardamos los datos del cliente
+  guardar(): void {
+
+    if (this.grupo == 'Sin definir') {
+      this.grupo = undefined;
+    }
+
+    let newCliente = {
+      "nombre": this.nombre,
+      "cif": this.cif,
+      "direccion": this.direccion,
+      "grupo": this.grupo
+    }
+    
+    this.clientes.push(newCliente);
+  }
+
+  // Elimina el último cliente
+  eliminarUltimo(): void {
+    this.clientes.pop();
+  }
 }
